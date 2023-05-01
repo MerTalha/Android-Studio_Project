d33f2599c1f0be6b4440c77c08675bfc14a1612b
MerTalha <merttalhasimsek@gmail.com>
2023-05-01 16:55:46 +0300

    i

--- Uygulama/app/src/main/java/com/example/depremharita/MainActivity.java
+++ Uygulama/app/src/main/java/com/example/depremharita/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.depremharita;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
+
+    private static final int ERROR_DIALOG_REQUEST = 9001;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public boolean isServiceOK(){
+        Log.d(TAG, "isServiceOK: checking google services version");
+        int available = Google
     }
 }
